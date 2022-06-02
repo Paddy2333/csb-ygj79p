12547832a0d03f8ee29af3982be47934394cad08
Code Sandbox <hello@codesandbox.io>
2022-06-02 02:48:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.3316084b5658974b9aa00e5a816a8701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f9b7ee157174a901517",
+    "url": "https://paddy2333.github.io/csb-ygj79p/static/css/main.80457914.chunk.css"
+  },
+  {
+    "revision": "5f9b7ee157174a901517",
+    "url": "https://paddy2333.github.io/csb-ygj79p/static/js/main.5f9b7ee1.chunk.js"
+  },
+  {
+    "revision": "0ba844e990f8fa19fcc7",
+    "url": "https://paddy2333.github.io/csb-ygj79p/static/js/1.0ba844e9.chunk.js"
+  },
+  {
+    "revision": "1aa56cdc90ef2db7ffec",
+    "url": "https://paddy2333.github.io/csb-ygj79p/static/js/runtime~main.1aa56cdc.js"
+  },
+  {
+    "revision": "9750a72541e94e717c4cb422f37f7d69",
+    "url": "https://paddy2333.github.io/csb-ygj79p/index.html"
+  }
+];
